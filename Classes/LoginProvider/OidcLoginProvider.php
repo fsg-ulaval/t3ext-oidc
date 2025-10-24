b3b06fc6a5b537e4476a1711b029559e63867083
Cyril Janody <cyril.janody@fsg.ulaval.ca>
2025-10-24 09:08:59 -0400

    fix

--- Classes/LoginProvider/OidcLoginProvider.php
+++ Classes/LoginProvider/OidcLoginProvider.php
@@ -9,7 +9,7 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 
 class OidcLoginProvider implements LoginProviderInterface
 {
-    public const int IDENTIFIER = 1754326802;
+    public const IDENTIFIER = 1754326802;
 
     public function render(StandaloneView $view, PageRenderer $pageRenderer, LoginController $loginController): void
     {
